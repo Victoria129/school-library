709cf2847d14abaac9bb974c14140529d19b0948
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-04-21 13:47:47 +0300

    Add decorators

--- person.rb
+++ person.rb
@@ -1,21 +1,23 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..500)
     @name = name
     @age = age
-    @parent_permission = parent_permission
-  end
-
-  attr_reader :id
-  attr_accessor :name, :age
-
-  private
-
-  def of_age?
+    # @parent_permission = parent_permission
     @age >= 18
   end
 
+  # attr_reader :id
+  # attr_accessor :name, :age
+
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
