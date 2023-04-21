51e7d4a1c3a30c36ecc52d0d1963d7a26e4af693
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-04-21 12:56:25 +0300

    check linters

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..500)
     @name = name
     @age = age
@@ -10,9 +10,11 @@ class Person
   attr_accessor :name, :age
 
   private
-  def of_age? @age >= 18
+  def of_age?
+    @age >= 18
   end
 
-  def can_use_services? of_age? || @parent_permission
+  def can_use_services?
+    of_age? || @parent_permission
   end
 end
