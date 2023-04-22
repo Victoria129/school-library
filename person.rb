4baacfbbd6ec5977f635198b0c34226761002396
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-04-22 14:10:08 +0300

    Set up associations

--- person.rb
+++ person.rb
@@ -1,30 +1,33 @@
 require './nameable'
+require './rental'
 
 class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..500)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
 
   private
 
   def of_age?
     @age >= 18
   end
-
   public
-
   def can_use_services?
     of_age? || @parent_permission
   end
-
   def correct_name
     @name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 end
